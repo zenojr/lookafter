a04bf266d8e6a54d2e63be96719f1cba8fd33428
Zeno França Junior <zfrancajunior@gmail.com>
2019-04-28 20:51:50 -0300

    firebase e git OK

---
+++ src/app/material.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatProgressSpinnerModule, MatFormFieldModule, MatInputModule, MatGridListModule,
+         MatToolbarModule, MatSelectModule, MatSnackBarModule, MatIconModule, MatStepperModule, MatDividerModule,
+         MatRadioModule, MatSidenavModule, MatListModule, MatAutocompleteModule, MatProgressBarModule, 
+         MatCardModule, MatExpansionModule } from '@angular/material';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+  CommonModule,
+  MatTableModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatGridListModule,
+  MatToolbarModule,
+  MatSelectModule,
+  MatSnackBarModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatStepperModule,
+  MatDividerModule,
+  MatRadioModule,
+  MatSidenavModule,
+  MatListModule,
+  MatAutocompleteModule,
+  MatProgressBarModule,
+  MatCardModule,
+  MatExpansionModule
+  ],
+  exports: [
+  MatTableModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatGridListModule,
+  MatToolbarModule,
+  MatSelectModule,
+  MatSnackBarModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatStepperModule,
+  MatDividerModule,
+  MatRadioModule,
+  MatSidenavModule,
+  MatListModule,
+  MatAutocompleteModule,
+  MatProgressBarModule,
+  MatCardModule,
+  MatExpansionModule
+  ]
+})
+export class MaterialModule { }
